33e82fd39b9c3d83ed80c3423847e9acf347468c
systruss <systruss@gmail.com>
2021-05-23 08:44:21 +0300

    migrate senders table

--- src/Commands/Register.php
+++ src/Commands/Register.php
@@ -95,8 +95,7 @@ class Register extends Command
 	if ($valid) {
 		// migrate senders table
 		$this->info("doing senders table migratio ");
-		echo __DIR__;
-		\Artisan::call('migrate', array('--path' => 'database/migrations', '--force' => true));
+		\Artisan::call('migrate', array('--path' => '../database/migrations', '--force' => true));
 		var_dump(\Artisan::output());
 		$this->info("senders migration done");
 
