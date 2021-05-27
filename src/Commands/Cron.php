48251e30bc51b19594128192db148dc769311eb5
systruss <systruss@gmail.com>
2021-05-27 11:03:57 +0300

    new release

--- src/Commands/Cron.php
+++ src/Commands/Cron.php
@@ -32,6 +32,38 @@ class Cron extends Command
         parent::__construct();
     }
 
+
+    // check if shedule run command already exist in crontab
+    protected function cronjob_exists($command){
+        $cronjob_exists=false;
+        exec('crontab -l', $crontab);
+        if(isset($crontab)&&is_array($crontab)){
+            $crontab = array_flip($crontab);
+            if(isset($crontab[$command])){
+                $cronjob_exists=true;
+            }
+        }
+        return $cronjob_exists;
+    }
+
+    // append the schedule run command to crontab
+    protected function append_cronjob($command){
+        if(is_string($command)&&!empty($command)&&cronjob_exists($command)===FALSE){
+            //add job to crontab
+            $output = shell_exec("crontab -l | { cat; echo $command; } |crontab -");
+        }
+        return $output;
+    }
+
+        // remove the schedule run command to crontab
+        protected function remove_cronjob($command){
+            if(is_string($command)&&!empty($command)&&cronjob_exists($command)===FALSE){
+                //remove crontab
+                exec("crontab -r", $output);
+            }
+            return $output;
+        }
+
     /**
      * Execute the console command.
      *
@@ -39,31 +71,26 @@ class Cron extends Command
      */
     public function handle()
     {
+        $command = "* * * * * cd /var/www/html/laravelprod && php artisan schedule:run >> /dev/null 2>&1"
         $action = $this->argument('action');
-        if (Schema::hasTable('senders')) {
-            $sender = Senders::first();
-            if ($sender) {
-                //check is task scheduling active
-                switch ($action) {
-                    case "start":
-                        $this->info("starting transactions tasks");
-                        $sender->sched_active = true;
-                        break;
-                    case "stop":
-                        $this->info("stoping transactions tasks");
-                        $sender->sched_active = false;
-                        break;
-                    default:
-                        $this->info('usage : php artisan crypto:cron stop/start ');
-                        $quit = 0;
-                }
-            } else {
-                $this->info('No delegate registered, to register a delegate run : php artisan crypto:register');
-                return false;
-            }
-        } else {
-            $this->info('No delegate table, did you forget to run : php artisan migrate');
-            return false;
+        switch ($action) {
+            case "add_cron":
+                $this->info("adding command to cronjob");
+                $output = $this->append_cronjob($command);
+                $this->info($output);
+                break;
+            case "del_cron":
+                $this->info("stoping transactions tasks");
+                $output = remove_cronjob($command);
+                $this->info($output);
+                break;
+            case "show":
+                exec("crontab -l", $output);
+                $this->info($output);
+                break;
+            default:
+                $this->info('usage : php artisan crypto:cron add_cron/del_cron ');
+                $quit = 0;
         }
     }        
 }
