f3a352be3a11d8d9c8f1ff90cb11a00257bade74
systruss <systruss@gmail.com>
2021-05-31 17:06:47 +0300

    new release

--- src/Services/Transactions.php
+++ src/Services/Transactions.php
@@ -24,8 +24,6 @@ use Systruss\SchedTransactions\Services\Server;
 
 
 const api_fee_url = "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/fees/fee.json";
-const api_delegates_edge_url ="https://api.hedge.infinitysolutions.io/api/delegates";
-const api_delegates_infi_url ="https://api.infinitysolutions.io/api/delegates";
 const MinDelegateBalance = 100000;
 const MinDelegateRank = 1;
 const MaxDelegateRank = 25;
@@ -51,65 +49,6 @@ class Transactions
 	public $transactions;
 	public $api_delegates_url;
 	public $publicKey;
-
-
-    public function checkDelegateEligibility(Delegate $delegate) 
-	{
-		$found = false;
-		$api_delegates_url = api_delegates_edge_url;
-
-		// check if delegate balance is grater than the minimum required
-		echo "\n delegate balance : $delegate->balance \n";
-		if ($delegate->balance < MinDelegateBalance) {
-			echo "\n insufficient balance \n";
-			return false;
-		}
-		// get list of delegate
-		$delegate_network = $delegate->network;
-
-
-		if ($delegate_network == "infi") {
-			echo "\n delegate network : $delegate_network \n";
-			$api_delegates_url = api_delegates_infi_url;
-		}
-
-		$client = new Client();
-		$res = $client->get($api_delegates_url);
-		if ($data = $res->getBody()->getContents()) 
-		{
-			$data = json_decode($data);
-			$totalDelegates = $data->meta->totalCount;
-			if ($totalDelegates > 0) {
-				$listDelegates = $data->data;
-				foreach ($listDelegates as $delegate_elem) {
-					if ($delegate_elem->address == $delegate->address) {
-						$this->rank = (int)$delegate_elem->rank;
-						$this->publicKey = $delegate_elem->publicKey;
-						$found = true;
-						break;
-					}
-				}
-				if ($found) {
-					echo "\n delegate rank : $rank \n";					
-					if ($rank >= MinDelegateRank && $rank <= MaxDelegateRank){
-						return true;
-					}else{
-						return false;
-					}
-				} else {
-					echo "\n delegate not found !!! \n";
-					return false;
-				}
-			} else {
-				echo "\n  number of delegate 0 !!! \n";
-				return false;
-			} 			
-		} else {
-				echo "\n no data returned from the api delagate url !!! \n";
-				return false;
-			}
-	}
-
 
 	public function getFee($network,$totalVoters)
 	{	
