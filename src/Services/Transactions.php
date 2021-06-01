3d0555b4be6b1c3112f44b93031c72d0d5fde485
systruss <systruss@gmail.com>
2021-06-01 13:05:37 +0300

    new release

--- src/Services/Transactions.php
+++ src/Services/Transactions.php
@@ -137,7 +137,12 @@ class Transactions
 			{
 				$generated = MultiPaymentBuilder::new();
 				foreach ($votersList->eligibleVoters as $voter) {
+					echo "\n ----------------- \n";
+					echo "\n voter portion : $voter['portion']  \n";
+					echo "\n delegate balance : $delegate->balance \n";
 					$amount = ($voter['portion'] * $delegate->balance) / 100;
+					echo "\n amount : $amount \n";
+					echo "\n ----------------- \n";
 					$generated = $generated->add($voter['address'], (int)$amount);
 				}
 				// add beneficary
