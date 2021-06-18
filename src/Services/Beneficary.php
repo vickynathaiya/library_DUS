1079d3efc533f5a2052b35edd1ac2f9332da029d
systruss <systruss@gmail.com>
2021-06-18 11:25:35 +0300

    new release

--- src/Services/Beneficary.php
+++ src/Services/Beneficary.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Systruss\SchedTransactions\Services;
+namespace Vickynathaiya\Dus\Services;
 
 use Illuminate\Console\Command;
 
 use ArkEcosystem\Crypto\Configuration\Network;
 use ArkEcosystem\Crypto\Identities\Address;
-use Systruss\SchedTransactions\Services\Networks\MainnetExt;
+use Vickynathaiya\Dus\Services\Networks\MainnetExt;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Schema;
 use GuzzleHttp\Client;
@@ -15,15 +15,11 @@ use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 use ArkEcosystem\Crypto\Transactions\Builder\TransferBuilder;
 use ArkEcosystem\Crypto\Transactions\Builder\MultiPaymentBuilder;
-use Systruss\SchedTransactions\Services\Delegate;
-use Systruss\SchedTransactions\Services\Server;
+use Vickynathaiya\Dus\Services\Delegate;
+use Vickynathaiya\Dus\Services\Server;
 
-const api_settings_delegate_url = "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/test_api/settings_delegate";
-
-const VoterMinBalance = 100000;
-const DelegateMinBalance = 100000;
-const minBalance = 100000;
-const DelegateAddress = "GeocWzPKN1kLWN4xCr4KWr75EBnkRS4ds1";
+# const api_settings_delegate_url = "https://raw.githubusercontent.com/InfinitySoftwareLTD/common/main/test_api/settings_delegate";
+const api_settings_delegate_url = "https://smartmarket.infinitysolutions.io/api/delegates";
 		
 
 class Beneficary 
@@ -33,53 +29,44 @@ class Beneficary
 	public $maintainMinimumBalance;
 	public $rate;
 	public $amount;
+	public $result;
 	
 	public function initBeneficary(Delegate $delegate) 
 	{
-		$found = false;
 		// get delegate address and network
 		$delegate_address = $delegate->address;
 		$delegate_network = $delegate->network;
+		$post_params = array('delegate_address'=>$delegate_address, 'network'=>$delegate_network);
 
 		// prepare api request to settings delegate
 		$client = new Client();
-		$res = $client->get(api_settings_delegate_url);
-		if ($data = $res->getBody()->getContents()) 
-		{
-			$data = json_decode($data);
-			switch ($delegate_network) {
-				case "infi":
-					$list_infi = $data->data->infi;
-					foreach ($list_infi as $delegate_address => $settings) {
-						if ($delegate_address == DelegateAddress) {
-							$this->address = $settings->beneficaryAddress;
-							$this->rate = $settings->beneficaryRate;
-							$this->requiredMinimumBalance = $settings->requiredMinimumBalance;
-							$this->maintainMinimumBalance = $settings->maintainMinimumBalance;
-							$found = true;
-							break;
-						}
+		try {
+			$res = $client->post(api_settings_delegate_url,['json'=> $post_params]);
+			$data = $req->getBody()->getContents();
+			if ($data)
+			{
+				$data = json_decode($data);
+				//treating data errors
+				if (isset($data->errors))
+				{
+					foreach ($data->errors as $error) {
+						$response['http_data'][] = is_object($error) ? $error : $error[0];
 					}
-					break;
-				case "edge":
-					$list_edge = $data->data->edge;
-
-					foreach ($list_edge as $delegate_address => $settings) {
-						if ($delegate_address == DelegateAddress) {
-							$this->address = $settings->beneficaryAddress;
-							$this->rate = $settings->beneficaryRate;
-							$this->requiredMinimumBalance = $settings->requiredMinimumBalance;
-							$found = true;
-							break;
-						}
-					}
-					break;
-				default:
-					echo "\n the delegate network is not correct \n";
-					$found = false;
-			} 
+					echo "\n (Benificiary) Failed to get delegate settings api \n";
+					$this->result = json_encode($response);
+					return false;
+				}
+				$this->address = $settings->beneficiaryAddress;
+				$this->rate = $settings->beneficiaryRate;
+				$this->requiredMinimumBalance = $settings->requiredMinimumBalance;
+				$this->maintainMinimumBalance = $settings->maintainMinimumBalance;
+				return true;
+			}
+		} catch (RequestException $e) {
+			echo "\n (Benificiary) Failed to connect to delegate settings api . \n";
+			$response = $e->getResponse();
+			$this->result = $response->getBody()->getContents();
+			return false;
 		}
-
-		return $found;
 	}
 }
